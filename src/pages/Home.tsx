30efbe3cdb8cf0f826c8e98e1ad9a08dcdd811b9
pfarwick <patrick.farwick@gmail.com>
2023-01-03 00:59:38 +0800

    m

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -55,7 +55,7 @@ function Home() {
                     }                                
                 </div>       
         </div>
-{/*         {instagramData}    */}
+        {instagramData}   
         </>
 
     )
