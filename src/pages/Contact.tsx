d357fc177ca28163211c97168296ff66f163548c
pfarwick <patrick.farwick@gmail.com>
2023-02-05 23:37:49 +0800

    m

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,8 +3,7 @@ import "./Contact.css";
 export const Contact = () => (
   <div>
     <p className="contactTitle">Fill out this form and we will get back to you as long as we get it</p>
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+    <form name="contact" method="POST" data-netlify="true" >
       <label htmlFor="name">Name</label>
       <input type="text" id="name" name="name" />
       <label htmlFor="email">Email</label>
@@ -13,8 +12,8 @@ export const Contact = () => (
       <input type="phone" id="phone" name="phone" />
       <label htmlFor="message">Message</label>
       <textarea id="message" name="message" />
-      <input type="submit" value="Send message" />
       <button type="submit">Send message</button>
     </form>
   </div>
 );
+
